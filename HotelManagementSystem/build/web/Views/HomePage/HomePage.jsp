0e9eb85cfe2b34d59353453f997422748bea5e8f
Nam Phong <phongnnhe176274@fpt.edu.vn>
2025-12-02 16:34:57 +0700

    base project

---
+++ HotelManagementSystem/build/web/Views/HomePage/HomePage.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : HomePage
+    Created on : Dec 2, 2025, 4:28:22 PM
+    Author     : xuxum
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
---
+++ HotelManagementSystem/src/java/Controller/HomePage/HomePageController.java
@@ -0,0 +1,27 @@
+package Controller.HomePage;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author
+ */
+public class HomePageController extends HttpServlet {
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.getRequestDispatcher("Views/HomePage/HomePage.jsp").forward(request, response);
+    }
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+    }
+    
+}
