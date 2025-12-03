7a79c36da5c07bb86308ef01303004e4300e575c
Nam Phong <phongnnhe176274@fpt.edu.vn>
2025-12-03 19:01:52 +0700

    create booking

--- HotelManagementSystem/src/java/Controller/Booking/Customer/BookingController.java
+++ HotelManagementSystem/src/java/Controller/Booking/Customer/BookingController.java
@@ -7,31 +7,34 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet(name = "BookingController", urlPatterns = {"/booking", "/booking_confirm"})
+@WebServlet(name = "BookingController", urlPatterns = {"/booking", "/booking_confirm", "/my_booking"})
 public class BookingController extends HttpServlet {
-    
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-         String path = request.getServletPath();
+        String path = request.getServletPath();
 
         switch (path) {
             case "/booking" ->
-               request.getRequestDispatcher("Views/Booking/Customer/Booking.jsp").forward(request, response);
+                request.getRequestDispatcher("Views/Booking/Customer/Booking.jsp").forward(request, response);
 
             case "/booking_confirm" ->
                 request.getRequestDispatcher("Views/Booking/Customer/BookingConfirm.jsp").forward(request, response);
 
+            case "/my_booking" ->
+                request.getRequestDispatcher("Views/Booking/Customer/MyBooking.jsp").forward(request, response);
+
             default ->
                 response.sendError(404);
         }
-       
+
     }
-    
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+
     }
-    
+
 }
